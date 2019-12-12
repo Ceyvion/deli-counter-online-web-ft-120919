49b2bdd7b47812d45f23ddeaaef4631bb67d3e32
Ceyvion Biggs <ceyvionb@gmail.com>
2019-12-12 23:08:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ def line(array)
  end 
 end
 
-def take_a_number(katz_deli, name)
+def take_a_number(array, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
